8a5259b8232edf83263f7432956cb27034455e9f
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2024-06-08 13:19:00 -0500

    adding config files

---
+++ wayfire/waybar/scripts/keyhint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# "Change keyboard layout in" "~/.config/hypr/hyprland.conf" " " \
+
+yad --width=360 --height=550 \
+--center \
+--fixed \
+--title="Keybindings" \
+--no-buttons \
+--list \
+--column=Key: \
+--column=Description: \
+--column=Command: \
+--timeout=60 \
+--timeout-indicator=right \
+"+H" "Early OOM" "" \
+"=Ctrl+BTN_RIGHT" "Wrot" "" \
+"+Shift+BTN_RIGHT" "3d-Wrot" "" \
+"+F1" "Play_with_mpv" "" \
+"+F2" "Joshuto" "" \
+"+F3" "Micro" "" \
+"+F4" "Nvim" "" \
+"+F5" "Flawless-Cut" "" \
+"+F6" "Losslesscut" "" \
+"+F7" "Jdownloader2" "" \
+"+F8" "" "" \
+"+F9" "pacui" "" \
+"+F10" "dmenu" "" \
+"+F11" "dmenumedia" "" \
+"+F12" "dmenurecord" "" \
+"Ctrl/+Enter" "Full Terminal" "" \
+"+D" "Application Menu" "" \
+"+R" "Application Runner" "" \
+"C+A+Left" "Cube Left" "" \
+"C+A+Right" "Cube Right" "" \
+"+Ctrl+BTN_LEFT" "Fisheye" "" \
+"+Shift+M" "Magnify" "" \
+"+Shift+0" "Hide Cursor" "" \
+"+Ctrl+v" "Scale All Windows" "" \
+"Print" "Screenshot Interactive" "" \
+"Shift+Print" "Screenshot in 5sec" "" \
+"+Print" "Screenshot in 10" "" \
+"+X" "Power-Menu" "" \
+"+Shift+C" "Random Theme" "" \
+"+Shift+F" "Fullscreen" "" \
+"+Shift+F5" "Minimize" "" \
+"+Shift+F6" "Maximize" "" \
+"+Shift+F7" "Stay On Top" "" \
+"+Shift+F8" "Sticky" "" \
+"+Shift+F9" "Push Behind" "" \
+"+Shift+F10" "Show Desktop" "" \
+"+Spacebar" "Float" "" \
+"+P" "Colorpicker" "" \
+"+L" "Lockscreen" "" \
+"+N" "NetowrkManager" "" \
+"" " MagicsysRq Alt+SysRq+B/E/F/S/U" "" \
