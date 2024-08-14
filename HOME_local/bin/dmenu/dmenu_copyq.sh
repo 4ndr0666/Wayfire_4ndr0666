255c72840df205e9263048e18a67dee064dbe57c
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2024-08-14 12:27:00 -0500

    Auto-commit: 0 added, 6 modified, 0 deleted

--- HOME_local/bin/dmenu/dmenu_copyq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-copyq_list=$(copyq list | sed 's/\t.*//')
-selected=$(echo "$copyq_list" | dmenu -i -l 10 -p "Clipboard History:")
-
-if [ -n "$selected" ]; then
-    copyq select "$selected"
-fi
